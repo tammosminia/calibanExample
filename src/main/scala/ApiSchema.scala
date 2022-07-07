74fdedb83f87162d605423f10255ec8ef6e16056
Tammo <tammo.sminia@klm.com>
2022-07-07 16:18:40 +0200

    zio caliban

--- src/main/scala/ApiSchema.scala
+++ src/main/scala/ApiSchema.scala
@@ -1,16 +1,17 @@
 import Cat.Color
 import caliban.Value.StringValue
+import caliban.schema.Annotations.GQLDefault
 import caliban.schema.{GenericSchema, Schema}
 
 object ApiSchema extends GenericSchema[CatRepo] {
-  case class DurationArgs(maxLength: Int, pad: Boolean)
-  def shortName(name: String)(args: DurationArgs): String = name.substring(0, args.maxLength) + (if (args.pad) "..." else "")
+  case class ShortNameArgs(maxLength: Int, @GQLDefault("true") pad: Boolean)
+  def shortName(name: String)(args: ShortNameArgs): String = name.substring(0, args.maxLength) + (if (args.pad) "..." else "")
 
   implicit val colorSchema: Schema[Any, Color] = scalarSchema[Color]("CatColor", Some("the color of the cat"), None, c => StringValue(c.s))
   implicit val catSchema: Schema[CatRepo, Cat] = obj[CatRepo, Cat]("Cat")(
     implicit ft =>
       List(
-        fieldWithArgs[Cat, DurationArgs]("name")(cat => shortName(cat.name)),
+        fieldWithArgs("name")(cat => shortName(cat.name)),
         field("color")(_.color),
         field("tail")(_.tail),
         field("speed")(cat => CatRepo.getSpeed(cat.name))
