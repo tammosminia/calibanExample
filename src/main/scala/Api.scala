069df1ddce4b903d8d5a520929c13eed28499c9b
Tammo <tammo.sminia@klm.com>
2022-07-07 11:48:53 +0200

    zio caliban

--- src/main/scala/Api.scala
+++ src/main/scala/Api.scala
@@ -1,10 +1,6 @@
-import Api._
-import Cat.Color
-import caliban.Value.StringValue
-import caliban.schema.{ArgBuilder, Schema}
-import caliban.{AkkaHttpAdapter, CalibanError, GraphQL, GraphQLInterpreter, RootResolver}
-import zio.{RIO, Runtime, ULayer, ZIO, ZLayer}
+import caliban._
 import sttp.tapir.json.circe._
+import zio.{Runtime, ULayer, ZIO, ZLayer}
 
 class Api {
   val catRepoLayer: ULayer[CatRepo] = ZLayer.succeed(new CatRepo())
--- src/main/scala/ApiSchema.scala
+++ src/main/scala/ApiSchema.scala
@@ -5,8 +5,6 @@ import caliban.schema.{GenericSchema, Schema}
 object ApiSchema extends GenericSchema[CatRepo] {
   case class DurationArgs(maxLength: Int, pad: Boolean)
   def shortName(name: String)(args: DurationArgs): String = name.substring(0, args.maxLength) + (if (args.pad) "..." else "")
-//  def shortName(name: String)(i: (Int, Boolean)): String = name.substring(0, i._1) + (if (i._2) "..." else "")
-//  def shortName(name: String)(maxLength: Int, pad: Boolean): String = name.substring(0, maxLength) + (if (pad) "..." else "")
 
   implicit val colorSchema: Schema[Any, Color] = scalarSchema[Color]("CatColor", Some("the color of the cat"), None, c => StringValue(c.s))
   implicit val catSchema: Schema[CatRepo, Cat] = obj[CatRepo, Cat]("Cat")(
